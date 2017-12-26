42855e37da676247335e13ffec6b6941036aef8a
pionerdsd <34263362+pionerdsd@users.noreply.github.com>
2017-12-26 20:54:36 +0300

    Initial commit

---
+++ out/artifacts/JavaEE_war_exploded/js/index.js
@@ -0,0 +1,93 @@
+
+
+function main(form) {
+    if (testLogin(form)&&testEmail(form)&&testPassword(form)&&testPol(form)&&testCheck(form)){
+        return true;
+    }
+    return false;
+}
+
+
+function testLogin(form){
+    var login = form.newLogin.value;
+    if(!/^.{5,15}$/.test(login)){
+        alert("Логин должен состоять из 5-15 символов!");
+        return false;
+    }
+    return true;
+}
+
+
+function testPassword(form){
+    var password = form.newPassword.value;
+    var repeatPassword = form.ConfirmPassword.value;
+    if(!/^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(password)){
+        alert("Пароль не подходит!");
+        return false;
+    }
+    if(password != repeatPassword){
+        alert("Пароли не совпадают!");
+        return false;
+    }
+    return true;
+}
+
+
+function testPol(form){
+    var pol = form.pol.value;
+    if(pol == ""){
+        alert("Choose your gender please!");
+        return false;
+    }
+    return true;
+}
+
+
+function testEmail(form){
+    var email = form.email.value;
+
+    if(email == ""){
+        alert("Insert your Email!");
+        return false;
+    }
+    return true;
+}
+
+
+
+function testCheck(form){
+    var check = form.check;
+    var bol = false;
+    for(var i = 0; i < check.length; i++){
+        if(check[i].checked){
+            bol = true;
+            break;
+        }
+    }
+    if(bol){
+        return true;
+    }
+    alert("Выберите хоть один check");
+    return false;
+}
+
+function changeColor(ob){
+    var ChangeButtonColor = document.getElementById("ChangeButtonColor6");
+    var TextArea = document.getElementById("comment");
+    //var ChangeButtonColor = ob.form.glyphicon1;
+    //var TextArea = ob.form.area1;
+    if (ob.checked) {
+
+        ChangeButtonColor.style.color="red";
+        TextArea.disabled=false;
+    } else {
+        ChangeButtonColor.style.color="black";
+        TextArea.disabled=true;
+        TextArea.value=" ";
+
+    }
+
+
+
+}
+
---
+++ src/by/homework/servlets/FirstServlet.java
@@ -0,0 +1,79 @@
+package by.homework.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.jms.Session;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(value="/sendForm")
+public class FirstServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
+    }
+
+
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+
+        HttpSession session =  request.getSession(true);
+
+
+
+      /*  Map<String, String[]> map = request.getParameterMap();
+        for(Map.Entry<String, String[]> entry: map.entrySet()) {
+            out.print(entry.getKey());
+            for(String s : entry.getValue()) {
+                out.print(s);
+                out.print("<br>");
+            }
+        }
+*/
+		String login = request.getParameter("newLogin");
+        out.print(login);
+/*
+		String [] checkMas = request.getParameterValues("check");
+		for(String s: checkMas) {
+			out.print(s);
+			out.print("<br>");
+		}
+
+		*/
+
+	/*	out.print("<!DOCTYPE html>");
+		out.print("<html><head>");
+		out.print("<meta charset='UTF-8'>");
+		out.print("<title>Insert title here</title>");*/
+        //out.print("Hello world! Привет мир!");
+        //out.print("</head><body>");
+
+    }
+
+
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+
+
+
+}
